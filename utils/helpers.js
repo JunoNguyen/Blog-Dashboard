3eebe23a25e4ed2fea1220a45a278d6b063230ca
Juno <junonguyen27@gmail.com>
2022-02-06 20:33:39 -0600

    going to restart from fresh file

--- utils/helpers.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // Formats date to display correctly when passed into handlebars
-  format_date: date => {
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-  }
-};
