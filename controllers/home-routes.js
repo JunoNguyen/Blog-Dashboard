b8258e3d53b30ef69810c5d3bf374469992868b3
Juno <junonguyen27@gmail.com>
2022-02-06 22:26:10 -0600

    adding handlebars files

---
+++ controllers/home-routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+const sequelize = require("./config/config.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/config.js");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: "Super secret secret",
-  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
